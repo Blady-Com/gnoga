1bf493bfbe24c8e3397140fd1557fb9bf21743b5
David Botton <david@botton.com>
2014-09-28 15:36:21 -0400

    Console like auto scrolling view.

--- test/demo.adb
+++ test/demo.adb
@@ -1,10 +1,9 @@
 with Gnoga.Application.Multiuser;
 with Gnoga.Gui.Window;
-with Gnoga.Gui.View;
+with Gnoga.Gui.View.Console;
 with Gnoga.Gui.Base;
 with Gnoga.Gui.Element;
 with Gnoga.Gui.Element.Common;
-with Gnoga.Gui.Element.Multimedia;
 with Gnoga.Types;
 
 
@@ -17,8 +16,7 @@ procedure Demo is
    type App_Data is new Connection_Data_Type with
       record
          Main_Window : Window.Pointer_To_Window_Class;
-         Audio1      : Multimedia.Audio_Type;
-         Video1      : Multimedia.Video_Type;
+         Console     : View.Console.Console_View_Type;
       end record;
    type App_Access is access all App_Data;
 
@@ -26,7 +24,9 @@ procedure Demo is
    is
       App : App_Access := App_Access (Object.Connection_Data);
    begin
-      App.Audio1.Play;
+      For i in 1 .. 50 loop
+         App.Console.Put_Line ("Another line");
+      end loop;
    end On_Click;
 
    procedure On_Connect
@@ -35,27 +35,13 @@ procedure Demo is
    is
       App     : aliased App_Data;
       Play    : Common.Button_Type;
-      View    : Gnoga.Gui.View.View_Type;
    begin
       App.Main_Window := Main_Window'Unchecked_Access;
 
-      View.Create (Main_Window);
+      App.Console.Create (Main_Window);
 
-      Play.Create (View, "Play");
+      Play.Create (App.Console, "Play");
       Play.On_Click_Handler (On_Click'Unrestricted_Access);
-
-      View.New_Line;
-
-      App.Audio1.Create (Parent   => View,
-                         Source   => "http://www.teachittome.com/misc/IgeretHaramban.mp3",
-                         Controls => False,
-                         Preload  => True);
-
-      View.New_Line;
-
-      App.Video1.Create (Parent   => View,
-                         Source   => "http://www.mindbodycures.com/archive/Ankle.mp4",
-                         Controls => True);
 
       Application.Multiuser.Connection_Data (Main_Window, App'Unchecked_Access);
 

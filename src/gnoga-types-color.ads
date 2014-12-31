7162a75cf24f80b9fbd7d004bcddacbb2f354959
David Botton <david@botton.com>
2014-12-31 18:28:27 -0500

    Color constants from Jeff Carter

---
+++ src/gnoga-types-color.ads
@@ -0,0 +1,180 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                   GNOGA - The GNU Omnificent GUI for Ada                 --
+--                                                                          --
+--                    G N O G A . T Y P E S . C O L O R S                   --
+--                                                                          --
+--                                 S p e c                                  --
+--                                                                          --
+--                                                                          --
+--                     Copyright (C) 2014 Jeff Carter                       --
+--                                                                          --
+--  This library is free software;  you can redistribute it and/or modify   --
+--  it under terms of the  GNU General Public License  as published by the  --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any --
+--  later version. This library is distributed in the hope that it will be  --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    --
+--                                                                          --
+--  As a special exception under Section 7 of GPL version 3, you are        --
+--  granted additional permissions described in the GCC Runtime Library     --
+--  Exception, version 3.1, as published by the Free Software Foundation.   --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License and   --
+--  a copy of the GCC Runtime Library Exception along with this program;    --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see   --
+--  <http://www.gnu.org/licenses/>.                                         --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+--                                                                          --
+--                                                                          --
+-- For more information please go to http://www.gnoga.com                   --
+------------------------------------------------------------------------------
+
+package Gnoga.Types.Colors is
+   Alice_Blue              : constant String := "AliceBlue";
+   Antique_White           : constant String := "AntiqueWhite";
+   Aqua                    : constant String := "Aqua";
+   Aquamarine              : constant String := "Aquamarine";
+   Azure                   : constant String := "Azure";
+   Beige                   : constant String := "Beige";
+   Bisque                  : constant String := "Bisque";
+   Black                   : constant String := "Black";
+   Blanched_Almond         : constant String := "BlanchedAlmond";
+   Blue                    : constant String := "Blue";
+   Blue_Violet             : constant String := "BlueViolet";
+   Brown                   : constant String := "Brown";
+   Burly_Wood              : constant String := "BurlyWood";
+   Cadet_Blue              : constant String := "CadetBlue";
+   Chartreuse              : constant String := "Chartreuse";
+   Chocolate               : constant String := "Chocolate";
+   Coral                   : constant String := "Coral";
+   Cornflower_Blue         : constant String := "CornflowerBlue";
+   Cornsilk                : constant String := "Cornsilk";
+   Crimson                 : constant String := "Crimson";
+   Cyan                    : constant String := "Cyan";
+   Dark_Blue               : constant String := "DarkBlue";
+   Dark_Cyan               : constant String := "DarkCyan";
+   Dark_Golden_Rod         : constant String := "DarkGoldenRod";
+   Dark_Gray               : constant String := "DarkGray";
+   Dark_Green              : constant String := "DarkGreen";
+   Dark_Khaki              : constant String := "DarkKhaki";
+   Dark_Magenta            : constant String := "DarkMagenta";
+   Dark_Olive_Green        : constant String := "DarkOliveGreen";
+   Dark_Orange             : constant String := "DarkOrange";
+   Dark_Orchid             : constant String := "DarkOrchid";
+   Dark_Red                : constant String := "DarkRed";
+   Dark_Salmon             : constant String := "DarkSalmon";
+   Dark_Sea_Green          : constant String := "DarkSeaGreen";
+   Dark_Slate_Blue         : constant String := "DarkSlateBlue";
+   Dark_Slate_Gray         : constant String := "DarkSlateGray";
+   Dark_Turquoise          : constant String := "DarkTurquoise";
+   Dark_Violet             : constant String := "DarkViolet";
+   DeepPink                : constant String := "DeepPink";
+   Deep_Sky_Blue           : constant String := "DeepSkyBlue";
+   Dim_Gray                : constant String := "DimGray";
+   Dodger_Blue             : constant String := "DodgerBlue";
+   Fire_Brick              : constant String := "FireBrick";
+   Floral_White            : constant String := "FloralWhite";
+   Forest_Green            : constant String := "ForestGreen";
+   Fuchsia                 : constant String := "Fuchsia";
+   Gainsboro               : constant String := "Gainsboro";
+   Ghost_White             : constant String := "GhostWhite";
+   Gold_Deep_Sky_Blue      : constant String := "GoldDeepSkyBlue";
+   Golden_Rod              : constant String := "GoldenRod";
+   Gray                    : constant String := "Gray";
+   Green                   : constant String := "Green";
+   Green_Yellow            : constant String := "GreenYellow";
+   Honey_Dew               : constant String := "HoneyDew";
+   Hot_Pink                : constant String := "HotPink";
+   Indian_Red              : constant String := "IndianRed";
+   Indigo                  : constant String := "Indigo";
+   Ivory                   : constant String := "Ivory";
+   Khaki                   : constant String := "Khaki";
+   Lavender                : constant String := "Lavender";
+   Lavender_Blush          : constant String := "LavenderBlush";
+   Lawn_Green              : constant String := "LawnGreen";
+   Lemon_Chiffon           : constant String := "LemonChiffon";
+   Light_Blue              : constant String := "LightBlue";
+   Light_Coral             : constant String := "LightCoral";
+   Light_Cyan              : constant String := "LightCyan";
+   Light_Golden_Rod_Yellow : constant String := "LightGoldenRodYellow";
+   Light_Gray              : constant String := "LightGray";
+   Light_Green             : constant String := "LightGreen";
+   Light_Pink              : constant String := "LightPink";
+   Light_Salmon            : constant String := "LightSalmon";
+   Light_Sea_Green         : constant String := "LightSeaGreen";
+   Light_Sky_Blue          : constant String := "LightSkyBlue";
+   Light_Slate_Gray        : constant String := "LightSlateGray";
+   Light_Steel_Blue        : constant String := "LightSteelBlue";
+   Light_Yellow            : constant String := "LightYellow";
+   Lime                    : constant String := "Lime";
+   Lime_Green              : constant String := "LimeGreen";
+   Linen                   : constant String := "Linen";
+   Magenta                 : constant String := "Magenta";
+   Maroon                  : constant String := "Maroon";
+   Medium_Aqua_Marine      : constant String := "MediumAquaMarine";
+   Medium_Blue             : constant String := "MediumBlue";
+   Medium_Orchid           : constant String := "MediumOrchid";
+   Medium_Purple           : constant String := "MediumPurple";
+   Medium_Sea_Green        : constant String := "MediumSeaGreen";
+   Medium_Slate_Blue       : constant String := "MediumSlateBlue";
+   Medium_Spring_Green     : constant String := "MediumSpringGreen";
+   Medium_Turquoise        : constant String := "MediumTurquoise";
+   Medium_Violet_Red       : constant String := "MediumVioletRed";
+   Midnight_Blue           : constant String := "MidnightBlue";
+   Mint_Cream              : constant String := "MintCream";
+   Misty_Rose              : constant String := "MistyRose";
+   Moccasin                : constant String := "Moccasin";
+   Navajo_White            : constant String := "NavajoWhite";
+   Navy                    : constant String := "Navy";
+   Old_Lace                : constant String := "OldLace";
+   Olive                   : constant String := "Olive";
+   Olive_Drab              : constant String := "OliveDrab";
+   Orange                  : constant String := "Orange";
+   Orange_Red              : constant String := "OrangeRed";
+   Orchid                  : constant String := "Orchid";
+   Pale_Golden_Rod         : constant String := "PaleGoldenRod";
+   Pale_Green              : constant String := "PaleGreen";
+   Pale_Turquoise          : constant String := "PaleTurquoise";
+   Pale_Violet_Red         : constant String := "PaleVioletRed";
+   Papaya_Whip             : constant String := "PapayaWhip";
+   Peach_Puff              : constant String := "PeachPuff";
+   Peru                    : constant String := "Peru";
+   Pink                    : constant String := "Pink";
+   Plum                    : constant String := "Plum";
+   Powder_Blue             : constant String := "PowderBlue";
+   Purple                  : constant String := "Purple";
+   Red                     : constant String := "Red";
+   Rosy_Brown              : constant String := "RosyBrown";
+   Royal_Blue              : constant String := "RoyalBlue";
+   Saddle_Brown            : constant String := "SaddleBrown";
+   Salmon                  : constant String := "Salmon";
+   Sandy_Brown             : constant String := "SandyBrown";
+   Sea_Green               : constant String := "SeaGreen";
+   Sea_Shell               : constant String := "SeaShell";
+   Sienna                  : constant String := "Sienna";
+   Silver                  : constant String := "Silver";
+   Sky_Blue                : constant String := "SkyBlue";
+   Slate_Blue              : constant String := "SlateBlue";
+   Slate_Gray              : constant String := "SlateGray";
+   Snow                    : constant String := "Snow";
+   Spring_Green            : constant String := "SpringGreen";
+   Steel_Blue              : constant String := "SteelBlue";
+   Tan                     : constant String := "Tan";
+   Teal                    : constant String := "Teal";
+   Thistle                 : constant String := "Thistle";
+   Tomato                  : constant String := "Tomato";
+   Turquoise               : constant String := "Turquoise";
+   Violet                  : constant String := "Violet";
+   Wheat                   : constant String := "Wheat";
+   White                   : constant String := "White";
+   White_Smoke             : constant String := "WhiteSmoke";
+   Yellow                  : constant String := "Yellow";
+   Yellow_Green            : constant String := "YellowGreen";
+end Gnoga.Types.Colors;

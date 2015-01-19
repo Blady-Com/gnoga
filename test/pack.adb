d171b5462809a290c2599323b1685f947ee6cd64
David Botton <david@botton.com>
2015-01-18 19:22:39 -0500

    tests on default sizing

--- test/pack.adb
+++ test/pack.adb
@@ -1,6 +1,7 @@
 with Gnoga.Application.Singleton;
 with Gnoga.Gui.Base;
 with Gnoga.Gui.Element;
+with Gnoga.Gui.Element.Common;
 with Gnoga.Gui.Window;
 with Gnoga.Gui.View.Grid;
 with Gnoga.Types.Colors;
@@ -14,6 +15,15 @@ procedure Pack is
    Main_View   : Gnoga.Gui.View.Grid.Grid_View_Type;
    Split_View  : Gnoga.Gui.View.Grid.Grid_View_Type;
 
+   A_View   : Gnoga.Gui.View.View_Type;
+   A_Button : Common.Button_Type;
+
+   procedure On_Click (Object : in out Gnoga.Gui.Base.Base_Type'Class);
+
+   procedure On_Click (Object : in out Gnoga.Gui.Base.Base_Type'Class) is
+   begin
+      Split_View.Panel (2, 1).Background_Color (Yellow_Green);
+   end On_Click;
 begin
    Gnoga.Application.Open_URL;
 
@@ -36,9 +46,21 @@ begin
 
    Split_View.Create (Main_View.Panel (2, 2).all, Vertical_Split);
 
+   Split_View.Panel (1, 1).Box_Height ("50%");
    Split_View.Panel (1, 1).Background_Color (Purple);
 
    Split_View.Panel (2, 1).Background_Color (Brown);
+
+   A_View.Create (Split_View.Panel (1, 1).all);
+   A_View.Box_Height ("100%");
+   A_View.Box_Width ("100%");
+   A_View.Border;
+   A_View.Text_Alignment (Center);
+
+   A_View.New_Line;
+
+   A_Button.Create (A_View, "Press Me");
+   A_Button.On_Click_Handler (On_Click'Unrestricted_Access);
 
    Gnoga.Application.Singleton.Message_Loop;
 end Pack;

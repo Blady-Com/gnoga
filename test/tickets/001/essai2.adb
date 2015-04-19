6dd6f092c1df8e0525c981f2b52b23d89f3cc056
David Botton <david@botton.com>
2015-04-18 22:20:12 -0400

    #001 - Program_Error with Window_Type in a package specification

---
+++ test/tickets/001/essai2.adb
@@ -0,0 +1,7 @@
+with Gnoga.Gui.Window;
+with Essai2_Ext_Decl;
+procedure Essai2 is
+LocalW : Gnoga.Gui.Window.Window_Type;
+begin
+null;
+end Essai2;
---
+++ test/tickets/001/essai2_ext_decl.ads
@@ -0,0 +1,4 @@
+with Gnoga.Gui.Window;
+package Essai2_Ext_Decl is
+ExtW : Gnoga.Gui.Window.Window_Type;
+end Essai2_Ext_Decl;

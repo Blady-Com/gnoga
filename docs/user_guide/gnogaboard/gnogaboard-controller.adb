aa0e8a48eb0b8515293ae1340d05758d461cd0f2
Blady <blady-com@users.sourceforge.net>
2018-04-23 11:39:57 +0200

    Enhancement from David Trudgett which adds a protected object in GnogaBoard from Gnoga user guide example.

--- docs/user_guide/gnogaboard/gnogaboard-controller.adb
+++ docs/user_guide/gnogaboard/gnogaboard-controller.adb
@@ -4,7 +4,50 @@ with GnogaBoard.View;
 
 package body GnogaBoard.Controller is
 
-   Users : Gnoga.Gui.Base.Base_Type_Array;
+   protected Users is
+      procedure Add (View : in GnogaBoard.View.Default_View_Access);
+      procedure Remove (N : in Positive);
+      function Find_User (P : in out Gnoga.Gui.Base.Base_Type'Class) return Natural;
+      entry Update_User_Views (X1, Y1, X2, Y2 : in Integer);
+   private
+      U : Gnoga.Gui.Base.Base_Type_Array;
+   end Users;
+
+   protected body Users is
+
+      procedure Add (View : in GnogaBoard.View.Default_View_Access) is
+      begin
+         U.Append (Gnoga.Gui.Base.Pointer_To_Base_Class (View));
+      end Add;
+
+      procedure Remove (N : Positive) is
+      begin
+         U.Delete (N);
+      end Remove;
+
+      function Find_User (P : in out Gnoga.Gui.Base.Base_Type'Class) return Natural is
+      begin
+         return U.Find_Index (P'Unchecked_Access);
+      end Find_User;
+
+      entry Update_User_Views (X1, Y1, X2, Y2 : in Integer) when not U.Is_Empty is
+      begin
+         for I in U.First_Index .. U.Last_Index loop
+            declare
+               User_View : constant GnogaBoard.View.Default_View_Access :=
+                             GnogaBoard.View.Default_View_Access (U.Element (I));
+            begin
+               User_View.Draw (X1, Y1, X2, Y2);
+            exception
+               -- Ignore error on attempting to draw to already-invalidated view.
+               when others =>
+                  null;
+            end;
+         end loop;
+      end Update_User_Views;
+
+   end Users;
+
 
    procedure On_Change (Object : in out Gnoga.Gui.Base.Base_Type'Class);
    --  Called by a view when a new line segment is drawn by the user. It will
@@ -19,23 +62,16 @@ package body GnogaBoard.Controller is
 
       View : Default_View_Type renames Default_View_Type (Object);
    begin
-      for i in Users.First_Index .. Users.Last_Index loop
-         declare
-            User_View : constant GnogaBoard.View.Default_View_Access :=
-              GnogaBoard.View.Default_View_Access (Users.Element (i));
-         begin
-            User_View.Draw (View.X1, View.Y1, View.X2, View.Y2);
-         end;
-      end loop;
+      Users.Update_User_Views (View.X1, View.Y1, View.X2, View.Y2);
    end On_Change;
 
    procedure On_Destroy (Object : in out Gnoga.Gui.Base.Base_Type'Class) is
       use Gnoga.Gui.Base.Base_Type_Arrays;
 
-      N : constant Integer := Users.Find_Index (Object'Unchecked_Access);
+      N : constant Natural := Users.Find_User (Object);
    begin
       if N /= No_Index then
-         Users.Delete (N);
+         Users.Remove (N);
       end if;
    end On_Destroy;
 
@@ -50,7 +86,7 @@ package body GnogaBoard.Controller is
    begin
       View.Dynamic;
       View.Create (Main_Window);
-      Users.Append (Gnoga.Gui.Base.Pointer_To_Base_Class (View));
+      Users.Add (View);
       View.On_Change := On_Change'Access;
       View.On_Destroy_Handler (On_Destroy'Access);
    end Default;

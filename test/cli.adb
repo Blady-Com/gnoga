7d8c6105347778fd10e20679e1be02cb837e601d
Blady <blady-com@users.sourceforge.net>
2019-11-02 12:37:20 +0100

    CLI: avoid following exception, move main loop in a dedicated task. 2019-11-01 21:39:58.29 : HTTP Server Stopping 2019-11-01 21:39:58.30 : Error connection 2 closed by exception. 2019-11-01 21:39:58.30 : raised PROGRAM_ERROR

--- test/cli.adb
+++ test/cli.adb
@@ -8,27 +8,63 @@ procedure CLI is
    use Gnoga.Types;
    use Gnoga.Gui;
 
+   type App_Data;
+   type App_Access is access all App_Data;
+
+   task type Main_Loop is
+      entry Start (A : App_Access);
+   end;
+
    type App_Data is new Connection_Data_Type with record
       Main_Window : Window.Pointer_To_Window_Class;
       CLI         : Plugin.Ace_Editor.Console_IO.Console_IO_Type;
+      Main        : Main_Loop;
    end record;
-   type App_Access is access all App_Data;
+
+   task body Main_Loop is
+      App : App_Access;
+      S   : String (1 .. 20);
+      L   : Natural;
+   begin
+      select
+         accept Start (A : App_Access) do
+            App := A;
+         end ;
+      or
+         terminate;
+      end select;
+      loop
+         App.CLI.Put ("Enter: ");
+         App.CLI.Put ("Line :@" & App.CLI.Get_Line);
+         App.CLI.Put ("@");
+         App.CLI.New_Line;
+         App.CLI.Put_Line ("Enter exit to end session,");
+         App.CLI.Put ("quit to end app: ");
+         App.CLI.Get_Line (S, L);
+         App.CLI.Put ("Line (20 max):@" & S (S'First .. L));
+         App.CLI.Put_Line ("@");
+         exit when S (S'First .. L) = "exit" or S (S'First .. L) = "quit";
+      end loop;
+      App.CLI.Put_Line ("End.");
+      if S (S'First .. L) = "quit" then
+         Application.Multi_Connect.End_Application;
+      end if;
+      Gnoga.Log ("End " & App.Main_Window.Connection_ID'Image);
+   end;
 
    procedure On_Connect
      (Main_Window : in out Gnoga.Gui.Window.Window_Type'Class;
       Connection  :        access Gnoga.Application.Multi_Connect
-        .Connection_Holder_Type);
+      .Connection_Holder_Type);
 
    procedure On_Connect
      (Main_Window : in out Gnoga.Gui.Window.Window_Type'Class;
       Connection  :        access Gnoga.Application.Multi_Connect
-        .Connection_Holder_Type)
+      .Connection_Holder_Type)
    is
       pragma Unreferenced (Connection);
 
       App : constant App_Access := new App_Data;
-      S   : String (1 .. 20);
-      L   : Natural;
 
    begin
       Main_Window.Connection_Data (App);
@@ -42,19 +78,7 @@ procedure CLI is
       App.CLI.Word_Wrap;
       App.CLI.Wrap_Limit (40);
       App.CLI.Editor_Execute ("focus()");
-
-      loop
-         App.CLI.Put ("Enter: ");
-         App.CLI.Put ("Line :@" & App.CLI.Get_Line);
-         App.CLI.Put ("@");
-         App.CLI.New_Line;
-         App.CLI.Put ("Enter exit to end: ");
-         App.CLI.Get_Line (S, L);
-         App.CLI.Put ("Line (20 max):@" & S (S'First .. L));
-         App.CLI.Put_Line ("@");
-         exit when S (S'First .. L) = "exit";
-      end loop;
-      App.CLI.Put_Line ("End.");
+      App.Main.Start (App);
    end On_Connect;
 
 begin
@@ -63,8 +87,9 @@ begin
       Boot  => "debug.html");
 
    Application.Title ("Editor");
---     Application.HTML_On_Close
---       ("<b>Connection to Application has been terminated</b>");
+   --     Application.HTML_On_Close
+   --       ("<b>Connection to Application has been terminated</b>");
 
    Application.Multi_Connect.Message_Loop;
 end CLI;
+
